3abb43d7026780ff06c39309c2505c200e34ce63
Sam Gardner <samgardner20@gmail..com>
2022-11-15 12:30:40 -0500

    added some styling and some more formating

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/Display.css"
 
 class Display extends React.Component {
     // constructor(props){
@@ -7,30 +8,49 @@ class Display extends React.Component {
     // }
     renderResponsibilities() {
         return(
+            
             <ul>
-            {this.props.responsibilities.map((res) => (
-                <li>{res}</li>
-            ))}
-        </ul>
+                {this.props.responsibilities.map((res) => (
+                    <li key={res} className="responsibility_item">{res}</li>
+                ))}
+            </ul>
         )
     }
     render(){
         
         return(
-            <div>
-                <h1>{this.props.name}</h1>
-                <h5>{this.props.email}</h5>
-                <h5>{this.props.phone}</h5>
+            <div className="display">
+                <div className="general">
+                    <h1 className="name">{this.props.name}</h1>
+                    <h5 className="email">{this.props.email}</h5>
+                    <h5 className="phone">{this.props.phone}</h5>
+                </div>
 
-                <h3>{this.props.school}</h3>
-                <h3>{this.props.title_of_study}</h3>
-                <h3>{this.props.date_of_study}</h3>
+                {this.props.school !== "" && 
+                
+                <div className="education">
+                    <h2 className="education_title">Education:</h2>
+                    <h3 className="school">{this.props.school}</h3>
+                    <h4 className="study">Field of Study: {this.props.title_of_study}</h4>
+                    <h4 className="study_date">Graduaction Date:{this.props.date_of_study}</h4>
+                </div>}
+                
+                {this.props.company_name !== "" &&
+                <div className="experience">
+                    <h2>Experience:</h2>
+                    <h3 className="company_name">Company: {this.props.company_name}</h3>
+                    <h4 className="position">Position Held: {this.props.position_title}</h4>
 
-                <h3>{this.props.company_name}</h3>
-                <h3>{this.props.position_title}</h3>
-                <h3>Responsibilities:</h3>
-                {this.renderResponsibilities()}
-                <h3>{this.props.position_date}</h3>
+                    {this.props.responsibilities.length === 0 ? "" : 
+                    <div className="responsibilities">
+                        <h4 className="responsibility_title">Responsibilities:</h4>
+
+                        {this.renderResponsibilities()}
+                    </div>}
+                    <h4 className="position_date">Years worked at Company: {this.props.position_date}</h4>
+                    
+                </div>}
+                
 
                 
             </div>
